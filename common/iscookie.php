393295af357815ccf9a85633ed3afd7b1497cd0d
boyoon <boyoon8@gmai.com>
2017-10-31 11:40:40 +0900

    shortcut 로직 수정

---
+++ common/iscookie.php
@@ -0,0 +1,19 @@
+<?php 
+
+include_once "./cookie.php";
+$cookie_value = isset($_REQUEST["cookie_value"]) ? $_REQUEST["cookie_value"] : "";
+
+$result=array();
+$result['iscookie']=0;
+foreach($cookie_array as $key => $value){
+    
+    if($value===$cookie_value) {
+        $result['iscookie']=1;
+        exit(json_encode($result));
+        return;
+    }
+    
+}
+$cookie_array='';
+exit(json_encode($result));
+?>
